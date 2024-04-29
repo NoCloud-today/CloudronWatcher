bfabdf80f43be50989fd0232e2577bbb52305aa3
mulyukovaaa <amina-mulukova.2002@yandex.ru>
2024-04-29 12:09:44 +0000

    [amina] fix error, time, replace text

--- cloudron_monitor.py
+++ cloudron_monitor.py
@@ -31,10 +31,10 @@ from datetime import datetime
 config = configparser.ConfigParser()
 config.read("settings.ini")
 
-cloudron_api_key = config['CloudronAPI']['CLOUDRON_TOKEN']
-cloudron_domain = config["CloudronAPI"]["CLOUDRON_DOMAIN"]
-bash_command = config['Messanger']['COMMAND_CURL']
-message_template = config['Messanger']['MESSAGE_TEMPLATE']
+cloudron_api_key = config['GENERAL']['CLOUDRON_TOKEN']
+cloudron_domain = config['GENERAL']["CLOUDRON_DOMAIN"]
+bash_command = config['GENERAL']['NOTIFICATION_CMD']
+message_template = config['GENERAL']['NOTIFICATION_TEMPLATE']
 
 
 def get_cloudron_notifications() -> list:
@@ -48,13 +48,6 @@ def get_cloudron_notifications() -> list:
         list: A list of notifications received from the Cloudron API.
     """
 
-    if not cloudron_api_key or not cloudron_domain:
-        current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-        sys.stderr.write(
-            f"\033[41mConfiguration error: Check the CLOUDFRONT_TOKEN and CLOUDFRONT_DOMAIN environment variables. Time: {current_time}\033[0m\n")
-        sys.stderr.flush()
-        return []
-
     headers = {'Authorization': f'Bearer {cloudron_api_key}'}
     url = f"https://{cloudron_domain}/api/v1/notifications"
 
@@ -63,13 +56,13 @@ def get_cloudron_notifications() -> list:
         response.raise_for_status()
 
     except requests.exceptions.RequestException as e:
-        current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        current_time = datetime.now().astimezone().strftime("%Y-%m-%d %H:%M:%S")
         sys.stderr.write(
             f"\033[41mError receiving notifications: {e}. Time: {current_time}\n\033[0m\n")
         sys.stderr.flush()
-        return []
+        exit(1)
 
-    current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    current_time = datetime.now().astimezone().strftime("%Y-%m-%d %H:%M:%S")
     sys.stdout.write(
         f"\033[92mNotifications were successfully received via the Cloudron API. Time: {current_time}\n\033[0m\n")
     sys.stdout.flush()
@@ -89,7 +82,7 @@ def mark_notification_as_acknowledged(notification_id: str) -> None:
     """
 
     if not cloudron_api_key or not cloudron_domain:
-        current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        current_time = datetime.now().astimezone().strftime("%Y-%m-%d %H:%M:%S")
         sys.stderr.write(
             f"\033[90mConfiguration error: Check the CLOUDRON_TOKEN and CLOUDRON_DOMAIN environment variables. Time: {current_time}\033[0m\n")
         sys.stderr.flush()
@@ -105,13 +98,13 @@ def mark_notification_as_acknowledged(notification_id: str) -> None:
         response.raise_for_status()
 
     except requests.exceptions.RequestException as e:
-        current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        current_time = datetime.now().astimezone().strftime("%Y-%m-%d %H:%M:%S")
         sys.stderr.write(
             f"\033[41mError when marking a notification as read: {e}. Time: {current_time}\n\033[0m")
         sys.stderr.flush()
         return
 
-    current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    current_time = datetime.now().astimezone().strftime("%Y-%m-%d %H:%M:%S")
     sys.stdout.write(
         f"\033[92mNotifications #{notification_id} have been successfully marked as read. Time: {current_time}\n\033[0m\n")
     sys.stdout.flush()
@@ -119,17 +112,22 @@ def mark_notification_as_acknowledged(notification_id: str) -> None:
 
 
 if __name__ == '__main__':
-    responses = get_cloudron_notifications()
 
-    if responses == []:
+    if not cloudron_api_key or not cloudron_domain or not bash_command:
+        current_time = datetime.now().astimezone().strftime("%Y-%m-%d %H:%M:%S")
+        sys.stderr.write(
+            f"\033[41mConfiguration error: Check the environment variables. Time: {current_time}\033[0m\n")
+        sys.stderr.flush()
         exit(1)
 
+    responses = get_cloudron_notifications()
+
     for notification in responses['notifications']:
-        if not notification['acknowledged']:
+        if  notification['acknowledged']:
             message_content = message_template.replace(
-                "{id}", notification['id'].replace('`', ''))
+                "{id}", notification['id'])
             message_content = message_content.replace(
-                "{title}", notification['title'].replace('`', ''))
+                "{title}", notification['title'])
             message_content = message_content.replace(
                 "{creationTime}", datetime.fromisoformat(
                     notification['creationTime'].replace("Z", "+00:00")).strftime("%d %B %Y, %H:%M:%S"))
@@ -143,14 +141,14 @@ if __name__ == '__main__':
                 bash_command_message, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 
             if json.loads(process.stdout)['ok']:
-                current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+                current_time = datetime.now().astimezone().strftime("%Y-%m-%d %H:%M:%S")
                 sys.stdout.write(
                     f"\033[92m\nThe notification #{notification['id']} has been sent successfully. Time: {current_time}\n\033[0m{notification}\n")
 
                 mark_notification_as_acknowledged(notification['id'])
 
             else:
-                current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+                current_time = datetime.now().astimezone().strftime("%Y-%m-%d %H:%M:%S")
                 sys.stderr.write(
                     f"\033[41mThe notification #{notification['id']} was not sent successfully. Time: {current_time}\n\033[0m")
                 sys.stderr.write(f"\033[41m{process.stdout}\n\033[0m\n")
