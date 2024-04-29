94b789dcf587a5c921324865a7f7df44424d741d
mulyukovaaa <amina-mulukova.2002@yandex.ru>
2024-04-29 15:41:46 +0000

    add handler for not curl cmd

--- cloudron_monitor.py
+++ cloudron_monitor.py
@@ -8,13 +8,14 @@ and marking notifications as acknowledged in Cloudron.
 
 Features:
 - Retrieves notifications from the Cloudron.
-- Sends notifications using a specified cURL command.
+- Sends notifications using a command.
 - Marks notifications as acknowledged in the Cloudron.
 
 Environment Variables from setting.ini:
 - CLOUDRON_TOKEN: The API token for Cloudron.
 - CLOUDRON_DOMAIN: The domain of the Cloudron instance.
-- COMMAND_CURL: The bash command to send notifications.
+- NOTIFICATION_CMD: The bash command to send notifications.
+- NOTIFICATION_TEMPLATE: The message template.
 
 Usage:
 - Ensure the environment variables are set correctly.
@@ -31,10 +32,10 @@ from datetime import datetime
 config = configparser.ConfigParser()
 config.read("settings.ini")
 
-cloudron_api_key = config['GENERAL']['CLOUDRON_TOKEN']
-cloudron_domain = config['GENERAL']["CLOUDRON_DOMAIN"]
-bash_command = config['GENERAL']['NOTIFICATION_CMD']
-message_template = config['GENERAL']['NOTIFICATION_TEMPLATE']
+cloudron_api_key = config['MAIN']['CLOUDRON_TOKEN']
+cloudron_domain = config['MAIN']["CLOUDRON_DOMAIN"]
+bash_command = config['MAIN']['NOTIFICATION_CMD']
+message_template = config['MAIN']['NOTIFICATION_TEMPLATE']
 
 
 def get_cloudron_notifications() -> list:
@@ -81,13 +82,6 @@ def mark_notification_as_acknowledged(notification_id: str) -> None:
         notification_id (str): The ID of the notification to be marked as acknowledged.
     """
 
-    if not cloudron_api_key or not cloudron_domain:
-        current_time = datetime.now().astimezone().strftime("%Y-%m-%d %H:%M:%S")
-        sys.stderr.write(
-            f"\033[90mConfiguration error: Check the CLOUDRON_TOKEN and CLOUDRON_DOMAIN environment variables. Time: {current_time}\033[0m\n")
-        sys.stderr.flush()
-        return
-
     headers = {'Authorization': f'Bearer {cloudron_api_key}'}
     data = {"acknowledged": True}
 
@@ -111,6 +105,58 @@ def mark_notification_as_acknowledged(notification_id: str) -> None:
     return
 
 
+def curl_handler(process: subprocess.CompletedProcess) -> None:
+    """
+    Handles the response from a cURL command.
+
+    This method analyzes the output of the cURL command, checks the success of sending the notification, and if the notification
+    was successfully sent, marks it as read.
+
+    Parameters:
+        process (subprocess.CompletedProcess): The result of the cURL command execution.
+    """
+    try:
+        json_data = json.loads(process.stdout)
+
+        if json_data['ok']:
+            current_time = datetime.now().astimezone().strftime("%Y-%m-%d %H:%M:%S")
+            sys.stdout.write(
+                f"\033[92m\nThe notification #{notification['id']} has been sent successfully. Time: {current_time}\n\033[0m{notification}\n")
+            sys.stdout.flush()
+
+            mark_notification_as_acknowledged(notification['id'])
+
+        else:
+            current_time = datetime.now().astimezone().strftime("%Y-%m-%d %H:%M:%S")
+            sys.stderr.write(
+                f"\033[41mThe notification #{notification['id']} was not sent successfully. Time: {current_time}\n{process.stdout}\n\033[0m")
+            sys.stderr.flush()
+
+    except json.JSONDecodeError:
+        current_time = datetime.now().astimezone().strftime("%Y-%m-%d %H:%M:%S")
+        sys.stderr.write(
+            f"\033[41mThe notification #{notification['id']} was not sent successfully. Time: {current_time}\nNOTIFICATION_CMD error: Check the curl cmd. \n\033[0m")
+        sys.stderr.flush()
+
+
+def not_curl_handler(process: subprocess.CompletedProcess) -> None:
+    """
+    Handles the response from a command not using cURL.
+
+    This method is intended for handling responses from commands that do not use cURL to send notifications. It analyzes
+    the command output, assuming the successful sending of the notification, and marks it as read.
+
+    Parameters:
+        process (subprocess.CompletedProcess): The result of the command execution.
+    """
+    current_time = datetime.now().astimezone().strftime("%Y-%m-%d %H:%M:%S")
+    sys.stdout.write(
+        f"\033[92m\nThe notification #{notification['id']} has been sent successfully. Time: {current_time}\n\033[0m{notification}\n")
+    sys.stdout.flush()
+
+    mark_notification_as_acknowledged(notification['id'])
+
+
 if __name__ == '__main__':
 
     if not cloudron_api_key or not cloudron_domain or not bash_command:
@@ -123,7 +169,7 @@ if __name__ == '__main__':
     responses = get_cloudron_notifications()
 
     for notification in responses['notifications']:
-        if  notification['acknowledged']:
+        if not notification['acknowledged']:
             message_content = message_template.replace(
                 "{id}", notification['id'])
             message_content = message_content.replace(
@@ -132,23 +178,26 @@ if __name__ == '__main__':
                 "{creationTime}", datetime.fromisoformat(
                     notification['creationTime'].replace("Z", "+00:00")).strftime("%d %B %Y, %H:%M:%S"))
             message_content = message_content.replace(
-                "{MESSAGE}", notification['message'].replace('`', ''))
+                "{MESSAGE}", notification['message'])
 
+        if "curl" in bash_command:
             bash_command_message = bash_command.replace(
                 "{MESSAGE}", quote(message_content))
+        else:
+            bash_command_message = bash_command.replace(
+                "{MESSAGE}", message_content.replace('`', '\`'))
 
-            process = subprocess.run(
-                bash_command_message, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        process = subprocess.run(
+            bash_command_message, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 
-            if json.loads(process.stdout)['ok']:
-                current_time = datetime.now().astimezone().strftime("%Y-%m-%d %H:%M:%S")
-                sys.stdout.write(
-                    f"\033[92m\nThe notification #{notification['id']} has been sent successfully. Time: {current_time}\n\033[0m{notification}\n")
-
-                mark_notification_as_acknowledged(notification['id'])
-
+        if process.returncode == 0:
+            if "curl" in bash_command:
+                curl_handler(process)
             else:
-                current_time = datetime.now().astimezone().strftime("%Y-%m-%d %H:%M:%S")
-                sys.stderr.write(
-                    f"\033[41mThe notification #{notification['id']} was not sent successfully. Time: {current_time}\n\033[0m")
-                sys.stderr.write(f"\033[41m{process.stdout}\n\033[0m\n")
+                not_curl_handler(process)
+
+        else:
+            current_time = datetime.now().astimezone().strftime("%Y-%m-%d %H:%M:%S")
+            sys.stderr.write(
+                f"\033[41mThe notification #{notification['id']} was not sent successfully. Time: {current_time}\n{process.stderr}\n\033[0m")
+            sys.stderr.flush()
