fc551c2e292e4f679b937be27d0706cfd6156b21
Alexander Potemkin <alex.p@getzen.dev>
2024-05-22 17:47:59 +0200

    Update and rename cloudron_monitor.sh to run.sh

--- cloudron_monitor.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-SCRIPTDIR="$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")"
-cd $SCRIPTDIR
-source venv/bin/activate
-python3 cloudron_monitor.py 2>&1 | tee -a cloudron_monitor.log
-#python3 cloudron_monitor.py --debug 2>&1 | tee -a cloudron_monitor.log
---
+++ run.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+SCRIPTDIR="$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")"
+cd $SCRIPTDIR
+source venv/bin/activate
+python3 CloudronWatcher.py 2>&1 | tee -a CloudronWatcher.log
+#python3 CloudronWatcher.py --debug 2>&1 | tee -a CloudronWatcher.log
