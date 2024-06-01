a586f534bf3124235009e7415e7b7535c50d4da5
mulyukovaaa <amina-mulukova.2002@yandex.ru>
2024-06-01 15:37:05 +0300

    remove check a blocking file to avoid restarting

--- run.sh
+++ run.sh
@@ -1,15 +1,7 @@
 #!/bin/bash
-
-if [ -f "running.txt" ]; then
-  echo "This script has already been run before" >&2
-  exit 1
-fi
-
-touch "running.txt"
-
 SCRIPTDIR="$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")"
 cd $SCRIPTDIR
 source venv/bin/activate
 python3 CloudronWatcher.py 2>&1 | tee -a CloudronWatcher.log
 #python3 CloudronWatcher.py --debug 2>&1 | tee -a CloudronWatcher.log
-rm "running.txt"
+
